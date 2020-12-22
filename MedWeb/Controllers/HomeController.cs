258ad37a1dcc66265d596c91bdf9780625018693
dhruv09-Del <59442286+dhruv09-Del@users.noreply.github.com>
2020-12-22 14:59:37 +0530

    Add files via upload

---
+++ MedWeb/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedWeb.Models;
+using MedWeb.Repos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MedWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+       
+
+       
+        private readonly IMedicineRepo _medicineRepo;
+
+        public HomeController(IMedicineRepo medicineRepo)
+        {
+            
+            _medicineRepo = medicineRepo;
+        }
+
+        [HttpGet]
+        [Route("GetMedicineList")]
+        public IActionResult Get()
+        {
+            var medList = _medicineRepo.GetMedicinesList();
+            return Ok(medList);
+        }
+
+        [HttpPost]
+        [Route("AddMedicineDetail")]
+        public IActionResult Add([FromBody]Medicine medicine)
+        {
+            
+            var med = _medicineRepo.AddMedicineToList(medicine);
+            if (med == true)
+                return Ok("Added");
+            else
+                return NoContent();
+        }
+    }
+}
---
+++ MedWeb/Models/Medicine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedWeb.Models
+{
+    public class Medicine
+    {
+        [Required(ErrorMessage = "Medicine's name is required")]
+        [Display(Name = "Name")]
+        [StringLength(100, ErrorMessage = "Medicine's name is too long")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Brand's name is required")]
+        [Display(Name = "Brand")]
+        [StringLength(100, ErrorMessage = "Brand's name is too long")]
+        public string Brand { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Display(Name = "Price")]
+        [DataType(DataType.Currency, ErrorMessage = "Price format is not valid ")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "It is required")]
+        [Display(Name = "ExpiryDate")]
+        [DataType(DataType.Date, ErrorMessage = "Expiry Date format is not valid")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ExpiryDate { get; set; }
+
+        [Required(ErrorMessage = "It is required")]
+        [Display(Name = "Notes")]
+        public string Notes { get; set; }
+    }
+
+    public class MedList
+    {
+        public Medicine[] items { get; set; }
+    }
+}
